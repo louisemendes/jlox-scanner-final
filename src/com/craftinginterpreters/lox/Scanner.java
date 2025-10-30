c777acc32995fc8a5bd5914a80b9564457b924c4
raianny-cristina <109386642+raianny-cristina@users.noreply.github.com>
2025-10-30 17:30:55 -0300

    Criação do arquivo Scanner

---
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -0,0 +1,34 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    public Scanner(String source) {
+        this.source = source;
+    }
+
+    public List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        // Deixar vazio.
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+}
