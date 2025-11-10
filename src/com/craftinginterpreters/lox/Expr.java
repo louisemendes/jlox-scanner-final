c80d413ee4440739ed0fae3d42034f7f56774569
raianny-cristina <109386642+raianny-cristina@users.noreply.github.com>
2025-11-10 17:24:14 -0300

    Implementa a estrutura AST (Expr.java) e o padrão Visitor para as expressoes

---
+++ src/com/craftinginterpreters/lox/Expr.java
@@ -0,0 +1,83 @@
+package com.craftinginterpreters.lox;
+
+// Necessário para o campo 'List<Expr>' (futuramente para o parsing de statements)
+import java.util.List; 
+
+// A classe base abstrata para todas as expressões
+abstract class Expr {
+
+    // 5.2.3: Interface Visitor - Define um método para cada classe de expressão
+    interface Visitor<R> {
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+    }
+
+    // 5.2.2: Classes Geradas (Subclasses estáticas)
+
+    // Representa operações binárias (1 + 2)
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) { // Método accept para o Visitor
+            return visitor.visitBinaryExpr(this);
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
+
+    // Representa expressões agrupadas por parênteses ((1 + 2))
+    static class Grouping extends Expr {
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
+
+        final Expr expression;
+    }
+
+    // Representa valores literais (123, "string", true, nil)
+    static class Literal extends Expr {
+        Literal(Object value) {
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
+
+        final Object value;
+    }
+
+    // Representa operações unárias (!true, -1)
+    static class Unary extends Expr {
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
+
+        final Token operator;
+        final Expr right;
+    }
+
+    // 5.2.2: Método Abstrato de aceitação
+    abstract <R> R accept(Visitor<R> visitor);
+}
