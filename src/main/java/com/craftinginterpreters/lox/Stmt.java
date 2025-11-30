0452a628c3cbc1d3d4562e1ea735d52ab1720aa0
raianny-cristina <109386642+raianny-cristina@users.noreply.github.com>
2025-11-30 14:23:55 -0300

    Criação da estrutura inicial de Stmt

---
+++ src/main/java/com/craftinginterpreters/lox/Stmt.java
@@ -0,0 +1,52 @@
+package main.java.com.craftinginterpreters.lox;
+
+import java.util.List;
+
+abstract class Stmt {
+
+    interface Visitor<R> {
+        R visitExpressionStmt(Expression stmt);
+        R visitPrintStmt(Print stmt);
+        R visitVarStmt(Var stmt);
+        R visitBlockStmt(Block stmt);
+        R visitIfStmt(If stmt);
+    }
+
+    static class Expression extends Stmt {
+        final Expr expression;
+        Expression(Expr expression) { this.expression = expression; }
+        @Override <R> R accept(Visitor<R> visitor) { return visitor.visitExpressionStmt(this); }
+    }
+
+    static class Print extends Stmt {
+        final Expr expression;
+        Print(Expr expression) { this.expression = expression; }
+        @Override <R> R accept(Visitor<R> visitor) { return visitor.visitPrintStmt(this); }
+    }
+
+    static class Var extends Stmt {
+        final Token name;
+        final Expr initializer;
+        Var(Token name, Expr initializer) { this.name = name; this.initializer = initializer; }
+        @Override <R> R accept(Visitor<R> visitor) { return visitor.visitVarStmt(this); }
+    }
+
+    static class Block extends Stmt {
+        final List<Stmt> statements;
+        Block(List<Stmt> statements) { this.statements = statements; }
+        @Override <R> R accept(Visitor<R> visitor) { return visitor.visitBlockStmt(this); }
+    }
+
+    static class If extends Stmt {
+        final Expr condition;
+        final Stmt thenBranch;
+        final Stmt elseBranch; // may be null
+        If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+            this.condition = condition; this.thenBranch = thenBranch; this.elseBranch = elseBranch;
+        }
+        @Override <R> R accept(Visitor<R> visitor) { return visitor.visitIfStmt(this); }
+    }
+
+    abstract <R> R accept(Visitor<R> visitor);
+}
+
